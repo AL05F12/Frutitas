1f6af55caa7a01b8f13506c65e7c9a2c5294782e
Pastor Cisneros <pas_ada@hotmail.com>
2014-06-02 13:14:52 -0500

    Cuatro
    Una app de Frutitas

---
+++ js/acciones.js
@@ -0,0 +1,98 @@
+// JavaScript Document
+	$(document).ready(function(e){
+
+
+ //click 
+
+	$('#B_Tem').tap(function (e) {
+ 	 $.mobile.navigate( "#Rex" );
+	});
+	
+	$('#B_Pag').tap(function (e) {
+ 	 $.mobile.navigate( "#Pag" );
+	});
+	
+	$('#B_Sab').tap(function (e) {
+ 	 $.mobile.navigate( "#Sab" );
+	});
+	
+	$('#B_Ace').tap(function (e) {
+ 	 $.mobile.navigate( "#Ace" );
+	});
+		
+	$('#B_Ini').tap(function (e) {
+ 	 $.mobile.navigate( "#principal" );
+	});
+	
+	$('#B_Temp').tap(function (e) {
+ 	 $.mobile.navigate( "#Rex" );
+	});
+	
+	$('#B_Pagi').tap(function (e) {
+ 	 $.mobile.navigate( "#Pag" );
+	});
+	
+	$('#B_Sabi').tap(function (e) {
+ 	 $.mobile.navigate( "#Sab" );
+	});
+	
+	$('#B_Acer').tap(function (e) {
+ 	 $.mobile.navigate( "#Ace" );
+	});
+		
+	$('#B_Inic').tap(function (e) {
+ 	 $.mobile.navigate( "#principal" );
+	});
+	
+	$('#B_Tempo').tap(function (e) {
+ 	 $.mobile.navigate( "#Rex" );
+	});
+	
+	$('#B_Pagin').tap(function (e) {
+ 	 $.mobile.navigate( "#Pag" );
+	});
+	
+	$('#B_Sabia').tap(function (e) {
+ 	 $.mobile.navigate( "#Sab" );
+	});
+	
+	$('#B_Acerc').tap(function (e) {
+ 	 $.mobile.navigate( "#Ace" );
+	});
+		
+	$('#B_Inici').tap(function (e) {
+ 	 $.mobile.navigate( "#principal" );
+	});
+	
+	$('#B_Tempor').tap(function (e) {
+ 	 $.mobile.navigate( "#Rex" );
+	});
+	
+	$('#B_Pagina').tap(function (e) {
+ 	 $.mobile.navigate( "#Pag" );
+	});
+	
+	$('#B_Sabias').tap(function (e) {
+ 	 $.mobile.navigate( "#Sab" );
+	});
+	
+	$('#B_Acerca').tap(function (e) {
+ 	 $.mobile.navigate( "#Ace" );
+	});
+		
+	$('#B_Inicio').tap(function (e) {
+ 	 $.mobile.navigate( "#principal" );
+	});
+	
+	$(document).ready(function(e) {
+ document.addEventListener("deviceready",function(){
+  $('#B_Vibrar').tap(function(){
+    navigator.notification.vibrate(500);
+  });
+   $('#B_Beep').tap(function(){
+    navigator.notification.beep(1);
+  });
+ },false); //deviceready
+}); //document ready
+	
+});//ready
